f03e14d6351dcf1a3f8c8802179e4160bd547e98
harri37 <harrison.wills@outlook.com>
2022-08-27 07:43:33 +1000

    improved form ui

--- GetUpAlarm/AlarmForm.js
+++ GetUpAlarm/AlarmForm.js
@@ -1,7 +1,9 @@
 import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
 import React, {useState} from 'react';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DatePicker from 'react-native-date-picker';
+//import DateTimePicker from '@react-native-community/datetimepicker';
 import DropDownPicker from 'react-native-dropdown-picker';
+import {useForm, Controller} from 'react-hook-form';
 
 const AlarmForm = () => {
   const [date, setDate] = useState(new Date());
@@ -17,57 +19,79 @@ const AlarmForm = () => {
     {label: 'Memory', value: 'memory'},
   ]);
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate;
-    setShowDate(false);
-    setDate(currentDate);
-  };
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+    reset,
+  } = useForm({
+    defaultValues: {
+      name: '',
+      time: new Date(),
+      stopOption: 'standard',
+    },
+  });
 
-  const showMode = currentMode => {
-    if (Platform.OS === 'android') {
-      setShowDate(false);
-      // for iOS, add a button that closes the picker
-    }
-    setMode(currentMode);
-  };
+  function submit(data) {
+    console.log(data);
+  }
 
-  const showTimepicker = () => {
-    showMode('time');
-    setShowDate('true');
-  };
   return (
-    <View>
-      <TextInput style={styles.field} placeholder="Name" />
+    <View style={styles.container}>
+      <Controller
+        control={control}
+        name="name"
+        rules={{required: true}}
+        render={({onChange, value}) => (
+          <TextInput style={styles.field} placeholder="Name" value={value} />
+        )}
+      />
+      {errors.name && <Text>required.</Text>}
+      <Controller
+        control={control}
+        name="time"
+        render={({onChange, value}) => (
+          <DatePicker
+            textColor="#ffffff"
+            date={date}
+            onDateChange={setDate}
+            mode="time"
+            androidVariant="nativeAndroid"
+            fadeToColor="none"
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="stopOption"
+        render={({onChange, value}) => (
+          <DropDownPicker
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+            style={styles.input}
+          />
+        )}
+      />
       <Button
-        onPress={showTimepicker}
-        title="Select Time"
+        title="create"
         style={styles.input}
+        onPress={() => handleSubmit(submit)}
       />
-      {showDate && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode={mode}
-          is24Hour={true}
-          onChange={onChange}
-          style={styles.input}
-        />
-      )}
-      <DropDownPicker
-        open={open}
-        value={value}
-        items={items}
-        setOpen={setOpen}
-        setValue={setValue}
-        setItems={setItems}
-        style={styles.input}
-      />
-      <Button title="create" style={styles.input} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    alignContent: 'center',
+    textAlign: 'center',
+    flex: 1,
+    color: 'white',
+  },
   field: {
     borderWidth: 2,
     borderColor: 'black',
