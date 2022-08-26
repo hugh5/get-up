d0ffcdc186146933400dd9669907a03b463ab81c
harri37 <harrison.wills@outlook.com>
2022-08-26 21:50:01 +1000

    added time select

--- GetUpAlarm/AlarmForm.js
+++ GetUpAlarm/AlarmForm.js
@@ -1,10 +1,44 @@
-import {View, Text, TextInput, StyleSheet} from 'react-native';
-import React from 'react';
+import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
+import React, {useState} from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const AlarmForm = () => {
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('time');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showMode = currentMode => {
+    if (Platform.OS === 'android') {
+      setShow(false);
+      // for iOS, add a button that closes the picker
+    }
+    setMode(currentMode);
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+    setShow('true');
+  };
   return (
     <View>
       <TextInput style={styles.field} placeholder="Enter alarm stuff" />
+      <Button onPress={showTimepicker} title="Show time picker!" />
+      <Text>selected: {date.toLocaleString()}</Text>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 };
