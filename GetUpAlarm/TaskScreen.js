110d4ab78139a83e1e681c124def3333bf07d3cd
hugh5 <hughdrummond11@gmail.com>
2022-08-28 00:46:51 +1000

    cancel button

--- GetUpAlarm/TaskScreen.js
+++ GetUpAlarm/TaskScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -7,13 +7,27 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 const TaskScreen = ({navigation}) => {
   return (
     <View>
-      <Text>Hello</Text>
+      <View style={styles.top}>
+        <Text>Hello</Text>
+      </View>
       <Button
+        style={styles.container}
         title="New Alarm"
         onPress={() => navigation.navigate('Set Alarms')}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  top: {
+    minHeight: 568,
+    maxHeight: 568,
+  },
+  container: {
+    height: 10,
+    marginTop: 500,
+  },
+});
 
 export default TaskScreen;
