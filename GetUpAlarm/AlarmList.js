2b83fbf4f26b2aee56a8813c66cc1758eca0933b
harri37 <harrison.wills@outlook.com>
2022-08-28 02:28:36 +1000

    whatever

--- GetUpAlarm/AlarmList.js
+++ GetUpAlarm/AlarmList.js
@@ -1,14 +1,30 @@
 import React, {useState} from 'react';
-import {View, Text, Button} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 function AlarmList({navigation}) {
   return (
-    <View
-      style={{flex: 1, color: 'black', textSize: 20, justifyContent: 'center'}}>
-      <Text>Alarm 1</Text>
-      <Text>Alarm 2</Text>
+    <View>
+      <View style={styles.top}>
+        <Text>Hello</Text>
+      </View>
+      <Button
+        style={styles.container}
+        title="New Alarm"
+        onPress={() => navigation.navigate('Set Alarms')}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  top: {
+    minHeight: 568,
+    maxHeight: 568,
+  },
+  container: {
+    height: 10,
+    marginTop: 500,
+  },
+});
 
 export default AlarmList;
