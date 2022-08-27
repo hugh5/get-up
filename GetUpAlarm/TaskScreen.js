dc146e0914b334d11e372d873393df796890e283
harri37 <harrison.wills@outlook.com>
2022-08-28 02:18:23 +1000

    added alarm store

--- GetUpAlarm/TaskScreen.js
+++ GetUpAlarm/TaskScreen.js
@@ -7,10 +7,6 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 const TaskScreen = ({navigation}) => {
   return (
     <View>
-<<<<<<< HEAD
-=======
-      <Text>Hello</Text>
->>>>>>> 1c73fdb3bdeb33c8efc0630aaa9095bd7b3d3bb8
       <Button
         title="New Alarm"
         onPress={() => navigation.navigate('Set Alarms')}
