30ac3288b33c25ccb218484b5cedf5e9dd7ae1fb
harri37 <harrison.wills@outlook.com>
2022-08-28 12:43:12 +1000

    alarm list functional

--- GetUpAlarm/AlarmList.js
+++ GetUpAlarm/AlarmList.js
@@ -13,44 +13,51 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-var print_list = [];
-
-for (let i = 0; i < 10; i++) {
-  print_list.push(
-    <View key={i}>
-      <Text style={{textAlign: 'left', marginTop: 5}}>{i}</Text>
-    </View>,
-  );
-}
-
 function AlarmList({navigation}) {
   importData = async () => {
     try {
       const keys = await AsyncStorage.getAllKeys();
       const result = await AsyncStorage.multiGet(keys);
-      console.log(result);
+      console.log('keys length', keys.length);
       return result;
     } catch (error) {
       console.error(error);
     }
   };
 
-  const [alarms, setAlarms] = useState();
+  const [alarms, setAlarms] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [alarmDisplay, setAlarmDisplay] = useState([]);
+  useEffect(() => {
+    setLoaded(false);
+  });
 
   useEffect(() => {
-    console.log('getting data');
-    importData().then(response => setAlarms(response));
-  }, []);
+    async function getAlarms() {
+      let alarmDisplayPartial = [];
+      const data = await importData();
+      setAlarms(data);
+      console.log('empty display', alarmDisplayPartial);
+      console.log('alarms', alarms);
+      alarms.forEach(element => {
+        const elementObj = JSON.parse(element[1]);
+        alarmDisplayPartial.push([
+          <Text style={styles.alarm} key={elementObj.name}>
+            {elementObj.name + ' , ' + elementObj.time.slice(11, 16)}
+          </Text>,
+        ]);
+      });
+      setAlarmDisplay(alarmDisplayPartial);
+      console.log('display', alarmDisplay);
+      setLoaded(true);
+    }
 
-  importData();
+    getAlarms();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-<<<<<<< HEAD
-      <ScrollView style={styles.scrollView}>{print_list}</ScrollView>
-=======
-      <ScrollView style={styles.scrollView}>{}</ScrollView>
->>>>>>> origin/harry
+      <ScrollView style={styles.scrollView}>{alarmDisplay}</ScrollView>
       <Button
         title="New Alarm"
         onPress={() => navigation.navigate('Set Alarms')}
@@ -60,6 +67,14 @@ function AlarmList({navigation}) {
 }
 
 const styles = StyleSheet.create({
+  alarm: {
+    fontSize: 50,
+    borderColor: 'black',
+    backgroundColor: 'white',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    marginBottom: 10,
+  },
   container: {
     flex: 1,
     paddingTop: StatusBar.currentHeight,
