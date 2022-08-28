4c61cd89d68692029d92d2df9531fbafae7349cd
hugh5 <hughdrummond11@gmail.com>
2022-08-28 10:54:48 +1000

    fixed styling again

--- GetUpAlarm/AlarmList.js
+++ GetUpAlarm/AlarmList.js
@@ -1,21 +1,51 @@
 import React, {useState} from 'react';
-import {View, Text, Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Button,
+  View,
+} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function AlarmList({navigation}) {
-  return (
-    <View
-      style={{flex: 1, color: 'black', textSize: 20, justifyContent: 'center'}}>
-      <Button
-        title="Create alarm"
-        onPress={() => navigation.navigate('Set Alarms')}
-      />
-      <Text>Alarm 1</Text>
-      <Text>Alarm 2</Text>
-    </View>
+var print_list = [];
+
+for (let i = 0; i < 10; i++) {
+  print_list.push(
+    <View key={i}>
+      <Text style={{textAlign: 'left', marginTop: 5}}>{i}</Text>
+    </View>,
   );
 }
+
+function AlarmList({navigation}) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>{print_list}</ScrollView>
+      <Button
+        title="New Alarm"
+        onPress={() => navigation.navigate('Set Alarms')}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 42,
+  },
+});
 
 export default AlarmList;
